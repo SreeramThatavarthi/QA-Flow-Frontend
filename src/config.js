e7a54971910c0dac8596acc781d39609561283d5
SreeramThatavarthi <56222315+SreeramThatavarthi@users.noreply.github.com>
2021-07-25 01:24:09 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
   googleClientId:
     "1072945288142-9l42v56jia9ja9bb8c7rr7jd7sj10rfh.apps.googleusercontent.com",
-  apiUrl: "https://qa-flow.herokuapp.com/",
+  apiUrl: "https://qa-flow.herokuapp.com",
 };
