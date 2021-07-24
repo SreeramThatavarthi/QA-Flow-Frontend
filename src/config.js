0f2e7c6849a57690ea0fd47df389846fd5e040a7
SreeramThatavarthi <56222315+SreeramThatavarthi@users.noreply.github.com>
2021-07-25 00:37:24 +0530

    Added Backend link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export const config = {
   googleClientId:
     "1072945288142-9l42v56jia9ja9bb8c7rr7jd7sj10rfh.apps.googleusercontent.com",
-  // apiUrl: "https://gconnect.asishraju.me",
-  apiUrl: "http://localhost:8000",
+  apiUrl: "https://qa-flow.herokuapp.com/",
 };
